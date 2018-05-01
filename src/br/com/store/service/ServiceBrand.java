5523562f451b2a004e35ba5b405f60917179cbb2
MurilloYamasaki <cracker021@hotmail.com>
2018-05-01 01:47:07 -0300

    Creation Of Product Service Classes
    Product, Brand, Category and SubCategory Service Classes

---
+++ src/br/com/store/service/ServiceBrand.java
@@ -0,0 +1,32 @@
+package br.com.store.service;
+
+import br.com.empresa.exception.DataSourceException;
+import br.com.store.db.dao.DAOBrand;
+import br.com.store.exception.BrandException;
+import br.com.store.model.Brand;
+import br.com.store.model.validator.ValidatorBrand;
+
+public class ServiceBrand {
+
+    private static final ServiceBrand INSTANCE = new ServiceBrand();
+
+    private ServiceBrand() {
+    }
+
+    public static ServiceBrand getInstance() {
+        return INSTANCE;
+    }
+
+    public void insert(Brand brand) throws BrandException, DataSourceException {
+        ValidatorBrand.validate(brand);
+
+        try {
+            DAOBrand.insert(brand);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new DataSourceException("Erro na fonte de dados");
+        }
+
+    }
+
+}
