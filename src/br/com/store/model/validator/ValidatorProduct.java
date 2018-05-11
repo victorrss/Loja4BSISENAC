0b6f5a2561e31ece0607f62cb5542859dae54af0
MurilloYamasaki <cracker021@hotmail.com>
2018-05-10 22:37:15 -0300

    Product Validator Fixed Up

--- src/br/com/store/model/validator/ValidatorProduct.java
+++ src/br/com/store/model/validator/ValidatorProduct.java
@@ -25,34 +25,34 @@ public class ValidatorProduct {
         if (product.getName() == null || product.getName().trim().isEmpty()) {
             throw new ProductException("Nome do produto não fornecido");
         }
-
-        if (product.getBarcode() == null || product.getBarcode().trim().isEmpty()) {
-            throw new ProductException("Código de barras do produto não fornecido");
-        }
-
-        if (product.getDescription() == null || product.getDescription().trim().isEmpty()) {
-            throw new ProductException("Descrição do produto não fornecida");
-        }
-
-        if (product.getWarranty() == null) {
-            throw new ProductException("Garantia do produto não fornecida");
-        }
-
-        if (product.getModel() == null || product.getModel().trim().isEmpty()) {
-            throw new ProductException("Modelo do produto não fornecido");
-        }
-
-        if (product.getPicture() == null) {
-            throw new ProductException("Foto do produto não fornecida");
-        }
-
-        if (product.getStock() == null) {
-            throw new ProductException("Estoque do produto não fornecido");
-        }
-
-        if (product.getPrice() == null) {
-            throw new ProductException("Preço do produto não fornecido");
-        }
+//
+//        if (product.getBarcode() == null || product.getBarcode().trim().isEmpty()) {
+//            throw new ProductException("Código de barras do produto não fornecido");
+//        }
+//
+//        if (product.getDescription() == null || product.getDescription().trim().isEmpty()) {
+//            throw new ProductException("Descrição do produto não fornecida");
+//        }
+//
+//        if (product.getWarranty() == null) {
+//            throw new ProductException("Garantia do produto não fornecida");
+//        }
+//
+//        if (product.getModel() == null || product.getModel().trim().isEmpty()) {
+//            throw new ProductException("Modelo do produto não fornecido");
+//        }
+//
+//        if (product.getPicture() == null) {
+//            throw new ProductException("Foto do produto não fornecida");
+//        }
+//
+//        if (product.getStock() == null) {
+//            throw new ProductException("Estoque do produto não fornecido");
+//        }
+//
+//        if (product.getPrice() == null) {
+//            throw new ProductException("Preço do produto não fornecido");
+//        }
 
     }
 }
